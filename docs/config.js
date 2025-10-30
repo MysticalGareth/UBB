1c9b6014b9d72642e016b034c485be956a0abe6d
Mystical Gareth <mysitcalgareth@proton.me>
2025-10-30 23:34:14 +0100

    Indexed @ 0000000000000000000172a6cbb9c16c91bbf6ad22f3b9fd71c7e8840b7526b1

--- docs/config.js
+++ docs/config.js
@@ -2,6 +2,6 @@
 window.UBB_CONFIG = {
   environment: "mainnet",
   genesisHash: "000000000000000000010fa5bf8de1bff433e934e03ed671186592c8c3560f6e",
-  tipHash: "00000000000000000000a060aa79b72fbec8feb5b4bffd72216dc5a4bd54e1ce",
+  tipHash: "0000000000000000000172a6cbb9c16c91bbf6ad22f3b9fd71c7e8840b7526b1",
   dataPath: "data/mainnet/v1/000000000000000000010fa5bf8de1bff433e934e03ed671186592c8c3560f6e"
 };
